0c6dcae04f90384615a45837ce5950021064872a
mkamruzzaman <mkamruzzaman@mangovoice.com>
2022-08-16 10:07:39 +0600

    updated PBXManager

---
+++ PBXManager/languages/en_us/Settings/PBXManager.php
@@ -0,0 +1,32 @@
+<?php
+
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+$languageStrings = array(
+    'LBL_SELECT_ONE' => 'Select',
+    'LBL_PBXMANAGER' =>'PBXManager',
+    'LBL_PBXMANAGER_CONFIG' => 'FusionPBX Server Details',
+    'LBL_NOTE' => 'Note:',
+    'LBL_INFO_WEBAPP_URL' => 'Configure your FusionPBX App URL in the format', 
+    'LBL_FORMAT_WEBAPP_URL' => '(protocol)://(fusionpbx_address):(port)',
+    'LBL_FORMAT_INFO_WEBAPP_URL' => 'ex : https://mypbx.net:8080',
+    'LBL_INFO_CONTEXT' => 'VFusion Connector', // - ?
+    'LBL_PBXMANAGER_INFO' => 'Configure FusionPBX Server Details after Installing VFusion Daemon in your Fusion Server', // - ?
+    
+    'webappurl'=>'FusionPBX URL',
+    'vtigersecretkey'=>'Vtiger API Key',
+    'outboundcontext' => 'Click2Call App path',
+    'outboundtrunk' => 'Fusion API Key',
+    
+);
+
+$jsLanguageStrings = array(
+    
+);
+?>  
---
+++ PBXManager/languages/ru_ru/Settings/PBXManager.php
@@ -0,0 +1,32 @@
+<?php
+
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * *********************************************************************************** */
+$languageStrings = array(
+    'LBL_SELECT_ONE' => 'Выберите',
+    'LBL_PBXManager' =>'PBXManager',
+    'LBL_PBXMANAGER_CONFIG' => 'Подробные сведения',
+    'LBL_NOTE' => 'Справка:',
+    'LBL_INFO_WEBAPP_URL' => 'Настройте URL FusionPBX в формате', 
+    'LBL_FORMAT_WEBAPP_URL' => '(Протокол)://(fusionpbx_ip): (порт)',
+    'LBL_FORMAT_INFO_WEBAPP_URL' => 'например: https://myfusionpbx.com:5000',
+    'LBL_INFO_CONTEXT' => 'VFusion Connector',
+    'LBL_PBXMANAGER_INFO' => 'Настройте эти поля после установки VFusion Daemon на Вашем FusionPBX сервере',
+    
+    'webappurl'=>'FusionPBX URL',
+    'vtigersecretkey'=>'Vtiger API ключ',
+    'outboundcontext' => 'Путь к Click2Call приложению',
+    'outboundtrunk' => 'Fusion API ключ',
+    
+);
+
+$jsLanguageStrings = array(
+    
+);
+?>  
